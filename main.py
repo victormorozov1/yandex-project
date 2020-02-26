f3d05b4a29aeb9117a9fa797b3d40bb60f0e02a9
Flashweb14 <kowalev.kirill2014@yandex.ru>
2020-02-26 18:18:24 +0500

    Добавление возможности перемещать карту

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ class Map:
     def __init__(self, coords, scale):
         self.coords = coords
         self.scale = scale
+        self.move_scale = float(self.scale.split(',')[0]) / 2
+
+    def handle_event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.move(self.move_scale, 0)
+            if event.key == pygame.K_DOWN:
+                self.move(-self.move_scale, 0)
+            if event.key == pygame.K_RIGHT:
+                self.move(0, self.move_scale)
+            if event.key == pygame.K_LEFT:
+                self.move(0, -self.move_scale)
 
     def get_coords(self, address):
         api_address = 'https://geocode-maps.yandex.ru/1.x'
@@ -23,30 +35,37 @@ class Map:
             print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
-
     def get_map(self):
         api_address = "https://static-maps.yandex.ru/1.x/?"
         response = requests.get(f'{api_address}l=map&ll={self.coords}&spn={self.scale}')
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
-        return map_file
+        if response:
+            map_file = "map.png"
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+            return map_file
+        return False
 
-    def move_map(self):
-        pass
+    def move(self, delta_y, delta_x):
+        coords = [float(coord) for coord in self.coords.split(',')]
+        coords[0] += delta_x
+        coords[1] += delta_y
+        self.coords = ','.join([str(coord) for coord in coords])
 
 
-map = Map('54.689,55.879', '0.1,0.1')
+map = Map('37.61556,55.75222', '1,1')
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map.get_map()), (0, 0))
-pygame.display.flip()
+if map.get_map():
+    screen.blit(pygame.image.load(map.get_map()), (0, 0))
+    pygame.display.flip()
 is_running = True
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.key == pygame.K_UP:
-                pass
+        if event.type == pygame.KEYDOWN:
+            map.handle_event(event)
+            if map.get_map():
+                screen.blit(pygame.image.load(map.get_map()), (0, 0))
+                pygame.display.flip()
 pygame.quit()
