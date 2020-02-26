bb870bd8e30390bfea7ed2d2f6c8b843b6a8b221
Flashweb14 <kowalev.kirill2014@yandex.ru>
2020-02-26 17:36:35 +0500

    :feature: Добаваление возможности выводить карту по программно заданным координатам и масштабу

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import sys
+import pygame
+import os
+
+
+def get_coords(address):
+    api_address = 'https://geocode-maps.yandex.ru/1.x'
+    api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
+    response = requests.get(f'{api_address}/?apikey={api_key}&' +
+                            f'geocode={address}, 1&format=json')
+    if response:
+        response_data = response.json()
+        geo_obj = response_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+        return ','.join(geo_obj["Point"]["pos"].split())
+    else:
+        print("Ошибка выполнения запроса")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+
+def get_map(coords, scale='0.1,0.1'):
+    api_address = "https://static-maps.yandex.ru/1.x/?"
+    response = requests.get(f'{api_address}l=map&ll={coords}&spn={scale}')
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    return map_file
+
+
+moscow_coords = get_coords('Москва')
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(get_map(moscow_coords)), (0, 0))
+pygame.display.flip()
+is_running = True
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+pygame.quit()
